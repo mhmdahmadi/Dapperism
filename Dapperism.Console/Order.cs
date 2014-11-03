d25ff325a3393d1737bec3829db3a390cb7e0dcf
hamedfathi <noeffect@outlook.com>
2014-11-03 23:06:54 +0330

    version 0.05

---
+++ Dapperism.Console/Order.cs
@@ -0,0 +1,27 @@
+using System;
+using Dapperism.Attributes;
+using Dapperism.Entities;
+using Dapperism.Enums;
+
+namespace Dapperism.Console
+{
+    [TableName("Orders")]
+    public class Order : Entity<Order>
+    {
+        [PrimaryKey(AutoNumber.Yes)]
+        public int OrderID { get; set; }
+        public string CustomerID { get; set; }
+        public int? EmployeeID { get; set; }
+        public DateTime? OrderDate { get; set; }
+        public DateTime? RequiredDate { get; set; }
+        public DateTime? ShippedDate { get; set; }
+        public int? ShipVia { get; set; }
+        public decimal? Freight { get; set; }
+        public string ShipName { get; set; }
+        public string ShipAddress { get; set; }
+        public string ShipCity { get; set; }
+        public string ShipRegion { get; set; }
+        public string ShipPostalCode { get; set; }
+        public string ShipCountry { get; set; }
+    }
+}
--- Dapperism.Console/Program.cs
+++ Dapperism.Console/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,8 +13,24 @@ namespace Dapperism.Console
         static void Main(string[] args)
         {
             var rep = new Repository<Order>();
+            var st1 = new Stopwatch();
+            st1.Start();
             var lst = rep.GetAll(2, 15);
+            st1.Stop();
 
+            var st2 = new Stopwatch();
+            st2.Start();
+            var lst2 = rep.GetAll(3, 15);
+            st2.Stop();
+
+            var st3 = new Stopwatch();
+            st3.Start();
+            var lst3 = rep.GetAll(4, 15);
+            st3.Stop();
+
+            var t1 = st1.ElapsedMilliseconds;
+            var t2 = st2.ElapsedMilliseconds;
+            var t3 = st3.ElapsedMilliseconds;
         }
     }
 }
