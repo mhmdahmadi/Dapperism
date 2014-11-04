08304a5ef7987515c2049bb476e4c2f5142f5537
hamedfathi <noeffect@outlook.com>
2014-11-05 00:15:54 +0330

    version 0.14

--- Dapperism.Console/Program.cs
+++ Dapperism.Console/Program.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using Dapperism.DataAccess;
-using Dapperism.Entities;
 using Dapperism.Enums;
 using Dapperism.Query;
 using Dapperism.Utilities;
